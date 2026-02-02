c68284fefb802897b37b0a0170385c7bf67263a6
eder000000 <eder000000@gmail.com>
2026-02-01 22:23:48 -0600

    add backgroud pics

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ window.DASH_CONFIG = {
   backgrounds: [
     "assets/bg-01.jpg",
     "assets/bg-02.jpg",
-    "assets/bg-03.jpg"
+    "assets/bg-03.jpg",
+    "assets/bg-04.jpg",
+    "assets/bg-05.jpg",
+    "assets/bg-06.jpg",
+    "assets/bg-07.jpg",
+    "assets/bg-08.jpg"
   ],
 
   backgroundRotateMs: 15 * 60 * 1000, // 15 min
